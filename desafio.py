c79f4025fcc182a9919d9573b551a8ab03c4397f
Alex Luna Pinheiro <57172346866@>
2025-04-15 17:01:39 -0300

    commit da aula

--- desafio.py
+++ desafio.py
@@ -4,20 +4,7 @@ jogadores = []
 critico = [1,1,1,1,1,1,1,2,1,1]
 
 hp = random.randint(200, 1000)
-while True:
-    vida = hp
-    dano = random.randint(1, 50)
-    defesa = random.randint(1, 50)
-    dano_oponente = random.randint(1, 50)
-    if defesa <= dano_oponente:
-        jogadores.append({
-            "nome": input("Digite o nome do Jogador 1: "),
-            "hp_total": hp,
-            "hp_atual": vida,
-            "dano": dano,
-            "defesa": defesa
-        })
-        break
+
 
 
 while True:
@@ -26,6 +13,20 @@ while True:
         exit()
     else:
         modo = int(input("DIGITE 1 PARA MULTIJOGADOR\nDIGITE 2 PARA JOGAR CONTRA A CPU\n"))
+        while True:
+            vida = hp
+            dano = random.randint(1, 50)
+            defesa = random.randint(1, 50)
+            dano_oponente = random.randint(1, 50)
+            if defesa <= dano_oponente:
+                jogadores.append({
+                    "nome": input("Digite o nome do Jogador 1: "),
+                    "hp_total": hp,
+                    "hp_atual": vida,
+                    "dano": dano,
+                    "defesa": defesa
+                })
+                break
         while True:
             vida = hp
             dano = dano_oponente
@@ -55,91 +56,106 @@ listaPocoes1 = ["1 - Super Força\n\n", "2 - Super cura\n\n", "3 - Super vida\n\
 listaPocoes2 = ["1 - Super Força\n\n", "2 - Super cura\n\n", "3 - Super vida\n\n", "4 - Pula Pula"]
 
 while jogadores[0]["hp_atual"] > 0 and jogadores[1]["hp_atual"] > 0:
-    rodada += 1
-    print(f"\n=== RODADA {rodada} ===")
+    multPocaoPula = 1
+    if multPocaoPula == 1:
+        rodada += 1
+        print(f"\n=== RODADA {rodada} ===")
+        for j in range(len(jogadores)):
+            if jogadores[j]["hp_atual"] <= 0:
+                continue
+            multPocaoDano = 1
+            multPocaoCura = 1
+            multPocaoVida = 0
+            pocaoUsada = 0
 
-    for j in range(len(jogadores)):
-        if jogadores[j]["hp_atual"] <= 0:
-            continue
 
-        print(f"\nTurno de {jogadores[j]['nome']}")
-        print(f"Seu HP: {jogadores[j]['hp_atual']} || HP do inimigo: {jogadores[1-j]['hp_atual']}")
 
-        multPocaoDano = 1
-        multPocaoCura = 1
-        multPocaoVida = 0
-        multPocaoPula = 1
-        pocaoUsada = 0
 
-        while True:
-            jogadores[j]["hp_atual"] += multPocaoVida
+            while True:
 
-            if jogadores[j]["nome"] != "CPU":
-                acao = int(input("\n1 - Atacar | 2 - Curar | 3 - Inventário: "))
 
-                if acao == 1:
-                    danoCritico = random.randint(0, 9)
-                    dano = (jogadores[j]["dano"] - jogadores[1-j]["defesa"]) * critico[danoCritico]
-                    dano = max(dano, 0)
-                    jogadores[1-j]["hp_atual"] -= dano
-                    print(f"\n{jogadores[j]['nome']} ataca! {jogadores[1-j]['nome']} perdeu {dano} de HP!")
-                    break
+                    print(f"\nTurno de {jogadores[j]['nome']}")
+                    print(f"Seu HP: {jogadores[j]['hp_atual']} || HP do inimigo: {jogadores[1 - j]['hp_atual']}")
+                    jogadores[j]["hp_atual"] += multPocaoVida
+                    texto = ""
+                    if jogadores[j]["nome"] != "CPU":
+                        acao = int(input("\n1 - Atacar | 2 - Curar | 3 - Inventário: "))
 
-                elif acao == 2:
-                    if jogadores[j]["hp_atual"] < jogadores[j]["hp_total"] - 20:
-                        jogadores[j]["hp_atual"] += (20 * multPocaoCura)
-                        print("Você se curou +20 HP!")
-                    else:
-                        cura = jogadores[j]["hp_total"] - jogadores[j]["hp_atual"]
-                        jogadores[j]["hp_atual"] += cura
-                        print(f"Você se curou +{cura} HP!")
-                    break
-
-                elif acao == 3:
-                    listaPocoes = listaPocoes1 if j == 0 else listaPocoes2
-                    if pocaoUsada == 1:
-                        print("Você já usou uma poção nessa rodada!")
-                    else:
-                        texto = "".join(listaPocoes)
-                        pocao = int(input(f"\nInventário:\n{texto}\n5 - Sair\n"))
-                        if pocao == 1 and "1 - Super Força\n\n" in listaPocoes:
-                            multPocaoDano = 2
-                            listaPocoes.remove("1 - Super Força\n\n")
-                            pocaoUsada += 1
-                        elif pocao == 2 and "2 - Super cura\n\n" in listaPocoes:
-                            multPocaoCura = 2
-                            listaPocoes.remove("2 - Super cura\n\n")
-                            pocaoUsada += 1
-                        elif pocao == 3 and "3 - Super vida\n\n" in listaPocoes:
-                            multPocaoVida = 50
-                            listaPocoes.remove("3 - Super vida\n\n")
-                            pocaoUsada += 1
-                        elif pocao == 4 and "4 - Pula Pula" in listaPocoes:
-                            multPocaoPula = 2
-                            listaPocoes.remove("4 - Pula Pula")
-                            pocaoUsada += 1
-                        elif pocao == 5:
+                        if acao == 1:
+                            danoCritico = random.randint(0, 9)
+                            dano = ((jogadores[j]["dano"] - jogadores[1-j]["defesa"]) * critico[danoCritico])*multPocaoDano
+                            dano = max(dano, 0)
+                            jogadores[1-j]["hp_atual"] -= dano
+                            print(f"\n{jogadores[j]['nome']} ataca! {jogadores[1-j]['nome']} perdeu {dano} de HP!")
                             break
+
+                        elif acao == 2:
+                            if jogadores[j]["hp_atual"] < jogadores[j]["hp_total"] - (20 * multPocaoCura):
+                                jogadores[j]["hp_atual"] += (20 * multPocaoCura)
+                                print("Você se curou +20 HP!")
+                            else:
+                                cura = jogadores[j]["hp_total"] - jogadores[j]["hp_atual"]
+                                jogadores[j]["hp_atual"] += cura
+                                print(f"Você se curou +{cura} HP!")
+                            break
+
                         else:
-                            print("Poção inválida ou já usada.")
-            else:
-                if multPocaoPula == 1:
-                    acaoCpu = random.randint(1, 2)
-                    if acaoCpu == 1:
-                        danoCritico = random.randint(0, 9)
-                        dano = (jogadores[j]["dano"] - jogadores[1-j]["defesa"]) * critico[danoCritico]
-                        dano = max(dano, 0)
-                        jogadores[1-j]["hp_atual"] -= dano
-                        print(f"{jogadores[j]['nome']} ataca! {jogadores[1-j]['nome']} perdeu {dano} de HP!")
+                            listaPocoes = listaPocoes1 if j == 0 else listaPocoes2
+                            if pocaoUsada == 1:
+                                print("Você já usou uma poção nessa rodada!\n\n")
+                            else:
+
+                                for i in listaPocoes:
+                                    texto = texto + i
+                                pocao = int(input(f"\nInventário:\n\n{texto}\n\n5- Sair"))
+                                if multPocaoDano != 2:
+                                    if pocao == 1:
+                                        multPocaoDano = 2
+                                        pocaoUsada +=1
+                                        listaPocoes.remove("1 - Super Força\n\n")
+                                else:
+                                    multPocaoDano = 1
+                                if multPocaoCura !=2:
+                                    if pocao == 2:
+                                        multPocaoCura = 2
+                                        pocaoUsada +=1
+                                        listaPocoes.remove("2 - Super cura\n\n")
+                                else:
+                                    multPocaoCura = 1
+                                if multPocaoVida != 50:
+                                    if pocao == 3:
+                                        multPocaoVida = 50
+                                        pocaoUsada +=1
+                                        listaPocoes.remove("3 - Super vida\n\n")
+                                else:
+                                    multPocaoVida = 0
+                                if multPocaoVida != 2:
+                                    if pocao == 4:
+                                        multPocaoPula = 2
+                                        pocaoUsada +=1
+                                        listaPocoes.remove("4 - Pula Pula")
+                                else:
+                                    multPocaoPula = 1
                     else:
-                        if jogadores[j]["hp_atual"] < jogadores[j]["hp_total"] - 20:
-                            jogadores[j]["hp_atual"] += 20
-                            print(f"{jogadores[j]['nome']} se curou +20 HP!")
-                        else:
-                            cura = jogadores[j]["hp_total"] - jogadores[j]["hp_atual"]
-                            jogadores[j]["hp_atual"] += cura
-                            print(f"{jogadores[j]['nome']} se curou +{cura} HP!")
-                    break
+                        if multPocaoPula == 1:
+                            acaoCpu = random.randint(1, 2)
+                            if acaoCpu == 1:
+                                danoCritico = random.randint(0, 9)
+                                dano = (jogadores[j]["dano"] - jogadores[1-j]["defesa"]) * critico[danoCritico]
+                                dano = max(dano, 0)
+                                jogadores[1-j]["hp_atual"] -= dano
+                                print(f"{jogadores[j]['nome']} ataca! {jogadores[1-j]['nome']} perdeu {dano} de HP!")
+                            else:
+                                if jogadores[j]["hp_atual"] < jogadores[j]["hp_total"] - 20:
+                                    jogadores[j]["hp_atual"] += 20
+                                    print(f"{jogadores[j]['nome']} se curou +20 HP!")
+                                else:
+                                    cura = jogadores[j]["hp_total"] - jogadores[j]["hp_atual"]
+                                    jogadores[j]["hp_atual"] += cura
+                                    print(f"{jogadores[j]['nome']} se curou +{cura} HP!")
+                            break
+
+
 
 if jogadores[0]["hp_atual"] > 0:
     print(f"\nParabéns {jogadores[0]['nome']}! Você venceu!")
